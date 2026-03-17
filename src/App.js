4328d9a364d30461b4c32be4474c0b2a7eda3c49
SavitriPun <punshakshi@gmail.com>
2026-03-17 23:02:56 +0000

    created Footer component and integrated Bootstrap into project

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
+import Footer from "./components/footer/Footer";
 import Weather from "./components/weather/Weather";
 
 function App() {
   return (
     <div className="App">
       <Weather />
+      <Footer />
     </div>
   );
 }

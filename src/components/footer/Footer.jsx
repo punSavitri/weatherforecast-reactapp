5c554ce9ea027477eac821f23bdec5ecffe037ca
SavitriPun <punshakshi@gmail.com>
2026-03-18 22:21:00 +0000

    added styling to Footer component, mounted it to App.js

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -2,7 +2,29 @@ import React from "react";
 import "./Footer.css";
 
 const Footer = () => {
-  return <div className="footer">Footer</div>;
+  return (
+    <div className="footer bg-light">
+      <p>
+        This project was developed by{" "}
+        <a
+          href="https://github.com/punSavitri"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Savitri Pun
+        </a>
+        {""} available on {""}
+        <a
+          href="https://github.com/punSavitri/weatherforecast-reactapp.git"
+          target="_blank"
+          rel="noreferrer"
+        >
+          as open-source on GitHub repository
+        </a>{" "}
+        and deployed using Hostinger platform
+      </p>
+    </div>
+  );
 };
 
 export default Footer;

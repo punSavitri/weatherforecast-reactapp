decb0e6ebd411688ff8ac8a50848c38138234a8f
SavitriPun <punshakshi@gmail.com>
2026-03-21 01:16:25 +0000

    added heading of react app

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Weather from "./components/weather/Weather";
 function App() {
   return (
     <div className="App">
+      <h1 className="text-center mt-3 pt-3">Weather Forecast App</h1>
       <Weather defaultCity="London" />
       <Footer />
     </div>

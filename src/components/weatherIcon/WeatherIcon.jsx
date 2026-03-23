659788ad4241dfea77a555c85bd2f1a87ac51244
SavitriPun <punshakshi@gmail.com>
2026-03-23 23:23:48 +0000

    create WeatherIcon component and integrate react-animated-weather for dynamic weather icon based on specific city weather

---
+++ src/components/weatherIcon/WeatherIcon.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import ReactAnimatedWeather from "react-animated-weather";
+
+const WeatherIcon = (props) => {
+  console.log(props.code);
+  const codeMapping = {
+    "clear-sky-day": "CLEAR_DAY",
+    "clear-sky-night": "CLEAR_NIGHT",
+    "few-clouds-day": "PARTLY_CLOUDY_DAY",
+    "few-clouds-night": "PARTLY_CLOUDY_NIGHT",
+    "scattered-clouds-day": "CLOUDY",
+    "scattered-clouds-night": "CLOUDY",
+    "broken-clouds-day": "PARTLY_CLOUDY_DAY",
+    "broken-clouds-night": "PARTLY_CLOUDY_NIGHT",
+    "shower-rain-day": "RAIN",
+    "shower-rain-night": "RAIN",
+    "rain-day": "RAIN",
+    "rain-night": "RAIN",
+    "thunderstorm-day": "THUNDERSTORM_DAY",
+    "thunderstorm-night": "THUNDERSTORM_NIGHT",
+    "snow-day": "SNOW",
+    "snow-night": "SNOW",
+    "mist-day": "MIST",
+    "mist-night": "MIST",
+  };
+  return (
+    <ReactAnimatedWeather
+      icon={codeMapping[props.code]}
+      color="1E1E1E"
+      size={64}
+      animate={true}
+    />
+  );
+};
+
+export default WeatherIcon;

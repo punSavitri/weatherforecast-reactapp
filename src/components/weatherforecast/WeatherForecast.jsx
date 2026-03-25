d62c2e882fe07e820e5bfe050077da8cb1d59e08
SavitriPun <punshakshi@gmail.com>
2026-03-25 09:33:32 +0000

    create WeatherForecast.jsx component to display forecast weather of city, mounted to Weather.jsx

---
+++ src/components/weatherforecast/WeatherForecast.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import './WeatherForecast.css'
+
+
+const WeatherForecast = () => {
+  return (
+    <div className='weatherforecast'>WeatherForecast</div>
+  )
+}
+
+export default WeatherForecast

53dcff4d13c963673193d3cf4c3223f084fa3bff
SavitriPun <punshakshi@gmail.com>
2026-03-27 09:43:42 +0000

    add logic in WeatherForecastDay to display single-day forecast based on user city input, mounted to WeatherForecast.jsx component

--- src/components/weatherforecast/WeatherForecastDay.jsx
+++ src/components/weatherforecast/WeatherForecastDay.jsx
@@ -2,16 +2,44 @@ import React from "react";
 import WeatherIcon from "../weatherIcon/WeatherIcon";
 import "./WeatherForecast.css";
 
-const WeatherForecastDay = () => {
+const WeatherForecastDay = (props) => {
+  console.log(props);
+
+  /*Extracts the maximum temperature from props, rounds it to the nearest whole number
+  and formats it with a degree symbol for display */
+  function maximumTemperature() {
+    let maxtemperature = Math.round(props.data.temperature.maximum);
+    //console.log(maxtemperature);
+    return `${maxtemperature}°`;
+  }
+
+  /*extracts the minimum temperature from props, rounds it to the nearest whole number,
+  and formats it with a degree symbol for display */
+  function minimumTemperature() {
+    let mintemperature = Math.round(props.data.temperature.minimum);
+    return `${mintemperature}°`;
+  }
+  /*returns the 3-letter weekday name (e.g "Mon") based on the given date*/
+  function day() {
+    const timestamp = props.data.time * 1000; //convert seconds into milliseconds
+    const date = new Date(timestamp);
+    const days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+    return days[date.getDay()];
+  }
+
   return (
     <div className="weatherforecast">
       <div className="row mt-4">
         <div className="col">
-          <div className="weatherforecast-day">WED</div>
-          <WeatherIcon code="few-clouds-night" size={36} />
+          <div className="weatherforecast-day">{day()}</div>
+          <WeatherIcon code={props.data.condition.icon} size={36} />
           <div>
-            <span className="weatherforecast-temperature-max">15°</span>
-            <span className="weatherforecast-temperature-min">7°</span>
+            <span className="weatherforecast-temperature-max">
+              {maximumTemperature()}
+            </span>
+            <span className="weatherforecast-temperature-min">
+              {minimumTemperature()}
+            </span>
           </div>
         </div>
       </div>
